b53ad55b33f0518c33a0750c7f70669e26e1be17
BettinaP <bettinaprophete@gmail.com>
2016-04-04 23:17:44 -0400

    simple array manips 2

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,65 +1,49 @@
 def using_push(array, string)
-  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-  next_country = "Niger"
-  countries_in_western_africa.push(next_country)
+  array.push(string)
 end
 
 def using_unshift(array, string)
-  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-  new_neighborhood = "Brooklyn Heights"
-  neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)  
+  array.unshift(string)
 end
 
 def using_pop(array)
-  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-  array.pop
+  return array.pop 
+array
 end
 
 def pop_with_args(array)
-  chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
-  array.pop(2)
+  return array.pop(2)
+  array
 end
 
 def using_shift(array)
-  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  array.shift
+  return array.shift 
 end
 
 def shift_with_args(array)
-  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  ice_cream_brands.shift(2)
+  array.shift(2)
 end
 
-def using_concat(array, array2)
-  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["mario kart", "flatiron school"]
- array.concat(array2)
+def using_concat(array1, array2)
+  array1.concat(array2)
 end
-
 
 def using_insert(array, element)
-  list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
-  another_esoteric_language = "Malbolge"
-  list_of_esoteric_programming_languages.insert(4, "Malbolge")
+  array.insert(4, element)
 end
 
-def using_uniq(array)
-  captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
-  captain_planet_and_the_planeteers.uniq!
+def using_uniq (array)
+  array.uniq 
 end
 
 def using_flatten(array)
-  private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
-  private_colleges_in_newyork.flatten
+  array.flatten 
 end
 
-def using_delete(array, string)
-  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  string = "Steven"
- array.delete(string)
+def using_delete(array, integer)
+  array.delete(integer)
 end
 
 def using_delete_at(array, integer)
-  famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
-  famous_robots.delete_at(2)
-end 
+  array.delete_at(integer)
+end
